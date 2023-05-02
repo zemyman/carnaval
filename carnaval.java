17494e9f17c9effabcd8fc37043d0bd862ab611d
zemyman <130682806+zemyman@users.noreply.github.com>
2023-05-02 13:28:23 -0400

    Update carnaval.java

--- carnaval.java
+++ carnaval.java
@@ -1,5 +1,64 @@
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class carnaval {
-public static void main(String[] args) throws Exception {
-    System.out.println("your a fag");
+    public static void main(String[] args) throws Exception {
+
+        JButton button = new JButton();
+        button.setText("click to play tic tac toe (2 players)");
+        button.setBounds(250, 250, 250, 50);
+        button.setFont(new Font("Comic Sans", Font.BOLD, 12));
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                // where game 1 goes
+            }
+        });
+
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.lightGray);
+       frame.add(tictactoeButton());
+       frame.add(gameSide());
+    }
+
+    public static JButton tictactoeButton(){
+        JButton button = new JButton();
+        button.setText("click to play tic tac toe (2 players)");
+        button.setBounds(250, 250, 250, 50);
+        button.setFont(new Font("Comic Sans", Font.BOLD, 12));
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                // where game 1 goes
+            }
+        });
+
+    return button;
 }
+
+
+    public static JPanel gameSide() {
+        JPanel games = new JPanel();
+        games.setBounds(0, 0, 250, 250);
+        games.setBackground(Color.blue);
+        games.setLayout(new GridBagLayout());
+        games.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        games.add(tictactoeButton());
+        return games;
+    }
+    
 }
