a77c35192d1e958f02d07dc5cf81142077f448f5
zemyman <130682806+zemyman@users.noreply.github.com>
2023-05-02 14:31:14 -0400

    day1 (userInterface)
    Today i just worked on the user Interface that the user will be interacting with. i made methods for buttons and i made a vending machine class with different sub class for diffrent options

--- carnaval.java
+++ carnaval.java
@@ -1,17 +1,23 @@
+import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class carnaval {
+   
+
     public static void main(String[] args) throws Exception {
 
         JPanel games = new JPanel();
@@ -20,6 +26,21 @@ public class carnaval {
         games.add(tictactoeButton());
         games.add(Button2());
 
+        StoreOp myStore = new StoreOp();
+        option2 myoption2 = new option2();
+
+       
+
+        JPanel shop = new JPanel();
+        shop.setBounds(0,250, 500, 200);
+        shop.setBackground(Color.red);
+        shop.setLayout(new BorderLayout());
+        shop.add(text(),  BorderLayout.PAGE_START);
+        shop.add(myStore.oP1(), BorderLayout.LINE_START);
+        shop.add(myoption2.oP2(),BorderLayout.CENTER);
+
+
+       
         JFrame frame = new JFrame();
         frame.setSize(500, 500);
         frame.setVisible(true);
@@ -27,7 +48,10 @@ public class carnaval {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.lightGray);
         frame.add(games);
+        frame.add(shop);
     }
+
+   
 
     public static JButton tictactoeButton() {
         JButton button = new JButton();
@@ -58,7 +82,49 @@ public class carnaval {
 
         return button1;
     }
+    public static JLabel text(){
+        JLabel text = new JLabel();
+        text.setText("Shop");
+        text.setVisible(true);
 
+        return text;
+
+    }
+
+}
+
+class StoreOp {
+    public JButton oP1() {
+        JButton option1 = new JButton();
+        option1.setText("food");
+        option1.setBounds(0, 50, 250, 50);
+        option1.setFont(new Font("Comic Sans", Font.BOLD, 12));
+        option1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                // food
+            }
+        });
+
+        return option1;
+    }
+
+}
+
+class option2 extends StoreOp {
    
+    public  JButton oP2() {
+        JButton option2 = new JButton();
+        option2.setText("drink");
+        option2.setBounds(0, 50, 250, 50);
+        option2.setFont(new Font("Comic Sans", Font.BOLD, 12));
+        option2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                //drink
+            }
+        });
 
+        return option2;
+    }
 }
